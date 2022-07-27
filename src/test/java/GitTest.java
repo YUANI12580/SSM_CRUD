29d1bc9fcc66abf1fca85fe101dc564b9e7e9247
yuan <yuan@local.com>
2022-07-27 17:30:56 +0800

    first commit

---
+++ src/test/java/GitTest.java
@@ -0,0 +1,13 @@
+/**
+ * @program: SSM_CRUD
+ * @description: 测试 Git
+ * @Author: Yuan
+ * @Date: 2022-07-27 17:29
+ **/
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+
+}
